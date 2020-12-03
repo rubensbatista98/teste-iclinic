7d2973f4991125823d01284e75e8206fbb328f4e
Rubens Batista <batistarubinho@gmail.com>
2020-12-03 15:20:53 -0300

    test(button): add Button component

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,42 @@
+import { renderWithTheme } from 'utils/tests/helpers';
+import theme from 'styles/theme';
+
+import Button from '.';
+
+describe('<Button />', () => {
+  test('should render Button with medium size by default', () => {
+    const { getByRole } = renderWithTheme(<Button>Hello</Button>);
+
+    expect(getByRole('button', { name: /hello/i })).toHaveStyle({
+      'font-size': theme.font.sizes.medium,
+      padding: `0px ${theme.spacings.medium}`
+    });
+  });
+
+  test('should render Button with large size', () => {
+    const { getByRole } = renderWithTheme(<Button size="large">Hello</Button>);
+
+    expect(getByRole('button', { name: /hello/i })).toHaveStyle({
+      'font-size': theme.font.sizes.large,
+      padding: `0px ${theme.spacings.xlarge}`
+    });
+  });
+
+  test('should render Button with uppercase content', () => {
+    const { getByRole } = renderWithTheme(<Button uppercase>Hello</Button>);
+
+    expect(getByRole('button', { name: /hello/i })).toHaveStyle({
+      'text-transform': 'uppercase',
+      'letter-spacing': '0.5rem'
+    });
+  });
+
+  test('should render a disabled Button', () => {
+    const { getByRole } = renderWithTheme(<Button disabled>Hello</Button>);
+
+    expect(getByRole('button', { name: /hello/i })).toHaveStyle({
+      opacity: '0.5',
+      'pointer-events': 'none'
+    });
+  });
+});
