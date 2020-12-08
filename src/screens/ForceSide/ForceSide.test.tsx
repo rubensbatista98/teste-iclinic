3a7d38438543d4356f01673842ef1abaf864cfff
Rubens Batista <batistarubinho@gmail.com>
2020-12-08 20:19:52 -0300

    test(force-side): ensure is redirect to home page if dont has or is not fetching data

--- src/screens/ForceSide/ForceSide.test.tsx
+++ src/screens/ForceSide/ForceSide.test.tsx
@@ -1,8 +1,22 @@
 import { screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import { renderWithProviders } from 'utils/tests/helpers';
+import {
+  renderWithProviders,
+  RenderProvidersConfig
+} from 'utils/tests/helpers';
 
 import ForceSide from '.';
+
+type RenderRouterConfig = {
+  route?: string;
+} & RenderProvidersConfig;
+
+const renderWithRouter = (config: RenderRouterConfig = {}) => {
+  window.history.pushState({}, 'Force Side', config?.route || '/force-side');
+
+  return renderWithProviders(<ForceSide />, { wrapper: Router, ...config });
+};
 
 describe('<ForceSide />', () => {
   test('should render with loading state if is fetching data initially', () => {
@@ -17,5 +31,18 @@ describe('<ForceSide />', () => {
 
     expect(screen.getByLabelText(/loading/i)).toBeInTheDocument();
     expect(container.childElementCount).toBe(1);
+  });
+
+  test('should redirect to home if has no data or if is not fetching', () => {
+    renderWithRouter({
+      sideProvider: {
+        updateSide: async () => undefined,
+        isLoading: false,
+        sideInfo: null,
+        isError: false
+      }
+    });
+
+    expect(window.location.pathname).toBe('/');
   });
 });
