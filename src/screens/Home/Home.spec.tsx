8bad8867fd873ddf1dd273256f944d2732cd3228
Rubens Batista <batistarubinho@gmail.com>
2020-12-08 19:08:10 -0300

    test(home): ensure when start button is clicked updateSide is called and go to force-side page

--- src/screens/Home/Home.spec.tsx
+++ src/screens/Home/Home.spec.tsx
@@ -1,12 +1,27 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import { renderWithProviders } from 'utils/tests/helpers';
+import {
+  renderWithProviders,
+  RenderProvidersConfig
+} from 'utils/tests/helpers';
 
 import Home from '.';
 
+type RenderRouter = {
+  route?: string;
+} & RenderProvidersConfig;
+
+const renderWithRouter = (config: RenderRouter = {}) => {
+  window.history.pushState({}, 'Home', config?.route || '/');
+
+  return renderWithProviders(<Home />, { wrapper: Router, ...config });
+};
+
 describe('<Home />', () => {
   test('should render Home correctly', () => {
-    renderWithProviders(<Home />);
+    renderWithRouter();
 
     expect(
       screen.getByRole('heading', { name: /welcome to iclinic/i })
@@ -17,5 +32,23 @@ describe('<Home />', () => {
     ).toBeInTheDocument();
 
     expect(screen.getByRole('button', { name: /start/i })).toBeInTheDocument();
+  });
+
+  test('should call updateSide and go to page force-side when start button is clicked', () => {
+    const updateSide = jest.fn();
+
+    renderWithRouter({
+      sideProvider: {
+        updateSide,
+        isError: false,
+        isLoading: false,
+        sideInfo: null
+      }
+    });
+
+    userEvent.click(screen.getByRole('button', { name: /start/i }));
+
+    expect(updateSide).toHaveBeenCalled();
+    expect(window.location.pathname).toBe('/force-side');
   });
 });
