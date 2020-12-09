353b67418a9b57d4dc86eb68922aebb9c1010fae
Rubens Batista <batistarubinho@gmail.com>
2020-12-09 18:53:08 -0300

    test(force-side): ensure renders the right screen when has luke or vader

--- src/screens/ForceSide/ForceSide.test.tsx
+++ src/screens/ForceSide/ForceSide.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -8,24 +9,27 @@ import {
 
 import ForceSide from '.';
 
-type RenderRouterConfig = {
-  route?: string;
-} & RenderProvidersConfig;
+type RenderRouterOptions = RenderProvidersConfig;
 
-const renderWithRouter = (config: RenderRouterConfig = {}) => {
-  window.history.pushState({}, 'Force Side', config?.route || '/force-side');
+const renderWithRouter = (options: RenderRouterOptions = {}) => {
+  const { ...config } = options;
 
-  return renderWithProviders(<ForceSide />, { wrapper: Router, ...config });
+  window.history.pushState({}, 'Force Side', '/force-side');
+
+  return renderWithProviders(<ForceSide />, {
+    wrapper: Router,
+    ...config
+  });
 };
 
 describe('<ForceSide />', () => {
   test('should render with loading state if is fetching data initially', () => {
-    const { container } = renderWithProviders(<ForceSide />, {
+    const { container } = renderWithRouter({
       sideProvider: {
         isError: false,
         isLoading: true,
         sideInfo: null,
-        updateSide: async () => undefined
+        updateSide: async () => void 0
       }
     });
 
@@ -33,10 +37,10 @@ describe('<ForceSide />', () => {
     expect(container.childElementCount).toBe(1);
   });
 
-  test('should redirect to home if has no data or if is not fetching', () => {
+  test('should redirect to home if does not has or is not fetching data', () => {
     renderWithRouter({
       sideProvider: {
-        updateSide: async () => undefined,
+        updateSide: async () => void 0,
         isLoading: false,
         sideInfo: null,
         isError: false
@@ -44,5 +48,49 @@ describe('<ForceSide />', () => {
     });
 
     expect(window.location.pathname).toBe('/');
+  });
+
+  test('should render the dark side screen if has Darth Vader as master', () => {
+    renderWithRouter({
+      sideProvider: {
+        isError: false,
+        isLoading: false,
+        sideInfo: {
+          master: 'Darth Vader',
+          side: 'dark'
+        },
+        updateSide: async () => void 0
+      }
+    });
+
+    expect(
+      screen.getByRole('heading', {
+        name: /your master is darth vader/i
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.getByAltText(/image of darth vader/i)).toBeInTheDocument();
+  });
+
+  test('should render the light side screen if has Luke Skywalker as master', () => {
+    renderWithRouter({
+      sideProvider: {
+        isError: false,
+        isLoading: false,
+        sideInfo: {
+          master: 'Luke Skywalker',
+          side: 'light'
+        },
+        updateSide: async () => void 0
+      }
+    });
+
+    expect(
+      screen.getByRole('heading', {
+        name: /your master is luke skywalker/i
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.getByAltText(/image of luke skywalker/i)).toBeInTheDocument();
   });
 });
